5f7963f118b087833d4703b88fae3f3c5aa5489c
Jessica Ho <jessicadinhho@gmail.com>
2022-05-27 01:21:50 -0400

    commit tree b3c8e62eb91d419123cc4f2d9d0260f701f41d10

---
+++ index.js
@@ -0,0 +1,29 @@
+//let number = 5
+function add(a,b){
+	return a + b;
+}
+add(5,7);
+function subtract(a,b){
+	return a - b;
+}
+subtract(5,7);
+function multiply(a,b){
+	return a * b;
+}
+multiply(5,7);
+function divide(a,b){
+	return a / b;
+}
+divide(5,7);
+function increment(n){
+	return n + 1;
+}
+function decrement(n){
+	return n - 1;
+}
+function makeInt(n){
+	return parseInt(n , 10);
+}
+function preserveDecimal(n){
+	return parseFloat(n , 10);
+} 
